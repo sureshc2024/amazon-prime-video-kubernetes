24af9eb97a27db555f5ba340d6b3a597c1a62608
MOHD ASEEM AKRAM <113539818+Aseemakram19@users.noreply.github.com>
2025-05-01 00:51:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "instance_name" {
 }
 
 variable "key_name" {
-  default = "---------------"                  # Names of key in aws
+  default = "cloudaseem"                  # Names of key in aws
 }
 
 
